dd576626f9c15bf2e75278a1e2d804145b22f497
mackmeggie <mack.meggie13@gmail.com>
2022-07-11 13:03:03 -0600

    create base objects for game

---
+++ rules.js
@@ -0,0 +1,27 @@
+
+const gameboard = (()=> {
+    const gameArray = [];
+    return {
+        gameArray,
+    };
+})();
+
+
+const players = (name, marker) => {
+    return {
+        name,
+        marker,
+    }
+}
+
+const displayController = (() => {
+    return{
+
+    };
+})();
+
+const gameplay = (() => {
+    return {
+
+    };
+})();
